9ab364fbc1eae903a4c9eed017d6d4a5920c12fd
taaaaak <a3ae10511@gmail.com>
2018-10-13 10:49:27 +0900

    create add page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'people/index'
+  get 'people', to: 'people#index'
+  get 'people/add'
+  post 'people/add', to: 'people#create'
   get 'people/:id', to: 'people#show'
   get 'dengonban/index'
 
